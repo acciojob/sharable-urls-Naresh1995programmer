bf4b41b6185424e76b9ceeb9a523efaa5c566b17
Naresh1995programmer <88931429+Naresh1995programmer@users.noreply.github.com>
2024-07-29 13:46:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // your code here
-document.addEventListemer('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
 	const urlForm = document.getElementById('urlForm');
 	const heading3 = document.getElementById('url');
 
-	urlForm.addEventListemer('submit', function(e){
-		event.preventDefault(); 
+	urlForm.addEventListener('submit', function(e){
+		e.preventDefault(); 
 		const nameValue = document.getElementById('name').value;
 		const yearValue = document.getElementById('year').value;
-		const newURL = `https://localhost:8080/?$name{encodeURIComponent(nameValue)}&year{encodeURIComponent(yearValue)}`
+		const newURL = `https://localhost:8080/?name=${encodeURIComponent(nameValue)}&year=${encodeURIComponent(yearValue)}`
 		heading3.textContent = newURL;
 	})
 })
