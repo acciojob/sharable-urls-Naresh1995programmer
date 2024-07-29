eca60226a95bf7f102014d2e19a59009cdf545fa
Naresh1995programmer <88931429+Naresh1995programmer@users.noreply.github.com>
2024-07-29 13:43:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.addEventListemer('DOMContentLoaded', function(){
 	const urlForm = document.getElementById('urlForm');
 	const heading3 = document.getElementById('url');
 
-	urlForm.addEventListemer('submit', function(){
+	urlForm.addEventListemer('submit', function(e){
+		event.preventDefault(); 
 		const nameValue = document.getElementById('name').value;
 		const yearValue = document.getElementById('year').value;
 		const newURL = `https://localhost:8080/?$name{encodeURIComponent(nameValue)}&year{encodeURIComponent(yearValue)}`
